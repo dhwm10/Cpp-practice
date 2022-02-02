a5c82c9b4dddeb98231f6ace0cd432533d0a6c2a
Joe Jamin <gksekausgksmsskawk@gmail.com>
2022-02-03 01:57:04 +0900

    CH1

---
+++ header1.h
@@ -0,0 +1,6 @@
+#include <iostream>
+namespace header1_name{
+    void var(){
+        std::cout<<"this is header1"<<std::endl;
+    }
+}
---
+++ header2.h
@@ -0,0 +1,6 @@
+#include <iostream>
+namespace header2_name{
+    void var(){
+        std::cout<<"this is header2"<<std::endl;
+    }
+}
